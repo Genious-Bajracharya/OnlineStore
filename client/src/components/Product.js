80ee6daf9793d6762b9379357fd5e444e3247e23
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2023-09-07 22:39:52 +0545

    Product desc new
    new product detail card

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-export default function Home() {
+export default function Product() {
   const [products, setProducts] = useState([]);
   const navigate = useNavigate();
 
@@ -13,15 +13,22 @@ export default function Home() {
 
   return (
     <div className="product-container">
-      <div className="container">
-        <div className="images">
-          <img src="http://mistillas.cl/wp-content/uploads/2018/04/Nike-Epic-React-Flyknit-%E2%80%9CPearl-Pink%E2%80%9D-01.jpg" />
-        </div>
+      <div className="product-image-container">
+        <img
+          className="product-image"
+          src="https://hebeboutique.com/cdn/shop/files/IMG_4174_750x.jpg?v=1689638857"
+          alt=""
+        />
+      </div>
 
-        <div className="product">
-          <h1>Nike Epic React Flyknit</h1>
-          <h2>$150</h2>
-          <p className="desc">too.</p>
+      <div className="product">
+        <h1 className="product-tile">Nike Epic React Flyknit</h1>
+        <span className="text-decoration-line-through">
+          ${products.price + 50}
+        </span>
+        <h2 className="product-price">$150</h2>
+        <p className="product-desc">too.</p>
+        <div className="product-button-div">
           <button className="add">Add to Cart</button>
         </div>
       </div>
--- client/src/components/Product_detail.js
+++ client/src/components/Product_detail.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import Product from "./Product";
 
-export default function Home() {
+export default function Product_detail() {
   const { id } = useParams();
   const [products, setProducts] = useState([]);
 
@@ -11,47 +12,25 @@ export default function Home() {
       .then((data) => setProducts(data));
   }, [id]);
   return (
-    <div>
-      <div>
-        <section className="py-5">
-          <div className="container px-4 px-lg-5 my-5">
-            <div className="row gx-4 gx-lg-5 align-items-center">
-              <div className="col-md-6">
-                <img
-                  className="card-img-top mb-5 mb-md-0"
-                  src={products.image}
-                  alt={products.title}
-                />
-              </div>
-              <div className="col-md-6">
-                <h1 className="display-5 fw-bolder">{products.title}</h1>
-                <div className="fs-5 mb-5">
-                  <span className="text-decoration-line-through">
-                    ${products.price + 50}
-                  </span>
-                  <span>${products.price}</span>
-                </div>
-                <p className="lead">{products.description}</p>
-                <div className="d-flex">
-                  <input
-                    className="form-control text-center me-3"
-                    id="inputQuantity"
-                    type="num"
-                    value="1"
-                    style={{ maxWidth: "3rem" }}
-                  />
-                  <button
-                    className="btn btn-outline-dark flex-shrink-0"
-                    type="button"
-                  >
-                    <i className="bi-cart-fill me-1" />
-                    Add to cart
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+    <div className="product-container">
+      <div className="product-image-container">
+        <img
+          className="product-image"
+          src={products.image}
+          alt={products.title}
+        />
+      </div>
+
+      <div className="product">
+        <h1 className="product-tile">{products.title}</h1>
+        <span className="text-decoration-line-through">
+          ${products.price + 50.0}
+        </span>
+        <h2 className="product-price">${products.price}</h2>
+        <p className="product-desc">{products.description}</p>
+        <div className="product-button-div">
+          <button className="add">Add to Cart</button>
+        </div>
       </div>
     </div>
   );
